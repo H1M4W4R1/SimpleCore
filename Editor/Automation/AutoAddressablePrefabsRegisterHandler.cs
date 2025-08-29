556eb33c992f5d508a284a5dd35399b909ed40e1
Patryk Pastuszka <nov11157@gmail.com>
2025-08-29 21:47:58 +0200

    Added support for [AutoAddressable] attribute

---
+++ Editor/Automation/AutoAddressablePrefabsRegisterHandler.cs
@@ -0,0 +1,44 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using JetBrains.Annotations;
+using Systems.SimpleCore.Automation.Attributes;
+using Systems.SimpleCore.Editor.Utility;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Systems.SimpleCore.Editor.Automation
+{
+    /// <summary>
+    ///     Script used to automatically register prefabs in Addressables system
+    /// </summary>
+    public sealed class AutoAddressablePrefabsRegisterHandler : AssetModificationProcessor
+    {
+        [NotNull] private static string[] OnWillSaveAssets([NotNull] string[] paths)
+        {
+            // Handle all assets
+            for (int pathIndex = 0; pathIndex < paths.Length; pathIndex++)
+            {
+                string path = paths[pathIndex];
+
+                Type assetType = AssetDatabase.GetMainAssetTypeAtPath(path);
+                
+                // Check if asset has proper attribute
+                AutoAddressableObjectAttribute attribute = assetType.GetCustomAttribute<AutoAddressableObjectAttribute>(true);
+                if (attribute == null) continue;
+                
+                // Get asset from path
+                Object asset = AssetDatabase.LoadAssetAtPath(path, assetType);
+                
+                // Register asset in Addressables system
+                AddressableExtensions.MarkAssetAddressable(path, attribute.Path, label: attribute.Label);
+            }
+
+            // Return unmodified paths
+            return paths;
+        }
+    }
+}
+#endif
